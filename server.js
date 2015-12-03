8d7155120e23197f58b5b60c929cdfc3aca97a3a
changhan <changhanlee@gmail.com>
2015-12-03 12:32:39 -0500

    404

--- server.js
+++ server.js
@@ -13,3 +13,13 @@ app.get("/game",function(req,res){
   var root = node();
   res.send(helper.pprint(root));
 });
+
+app.use(function(req, res, next){
+  res.status(404);
+  res.send("Wrong address");
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
