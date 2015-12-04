939e754fbfea64f91bce494e4239658918b77a35
changhan <changhanlee@gmail.com>
2015-12-04 11:59:48 -0500

    ok

--- src/helper/pretty.js
+++ src/helper/pretty.js
@@ -3,7 +3,9 @@ var pretty = {};
 pretty.print = function(node) {
   var pretty = node.board.split("");
   pretty.forEach(function(y, x, arr) {
-    arr[x] += " ";
+    if (x < pretty.length-1){
+      arr[x] += " ";
+    }
   });
   pretty = pretty.join("");
   return pretty;
